7c92922cdebb1959acaaf7a05ef00d4fc771a932
mraikero-01 <blackantrax01@gmail.com>
2023-03-21 12:00:46 +0800

    added api function GET/POST

--- log.js
+++ log.js
@@ -1,5 +1,5 @@
-const chalk = require('chalk');
-module.exports = function(data, data2, option) {
-	if (option == "ok") return console.log(("-----------------------------------------------------------\n")+chalk.bold.hex("#FFFF00")('[ OK ] » ') + chalk.hex("#19ade1")(data) + chalk.hex("#00FF00")(data2));
-	if (option == "err") return console.log(("-----------------------------------------------------------\n")+chalk.bold.hex("#FF0000")('[ ERR ] » ') + chalk.hex("#19ade1")(data) + chalk.hex("#Efa70f")(data2));
-}
+const chalk = require("chalk");
+module.exports = function (data, data2, option) {
+    if (option == "ok") return console.log("-----------------------------------------------------------\n" + chalk.bold.hex("#FFFF00")("[ OK ] » ") + chalk.hex("#19ade1")(data) + chalk.hex("#00FF00")(data2));
+    if (option == "err") return console.log("-----------------------------------------------------------\n" + chalk.bold.hex("#FF0000")("[ ERR ] » ") + chalk.hex("#19ade1")(data) + chalk.hex("#Efa70f")(data2));
+};
